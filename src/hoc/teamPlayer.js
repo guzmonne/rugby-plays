efcccafaa083a57a2dcbb1c9c7a6cb36dd6e5abd
Guzman Monne <guzmonne@hotmail.com>
2017-05-16 16:42:57 -0300

    working on a new Draggable component

--- src/hoc/teamPlayer.js
+++ src/hoc/teamPlayer.js
@@ -1,23 +1,18 @@
 import React from 'react'
+import Draggable from '../components/common/Draggable.js'
 import Transform from '../components/common/Transform.js'
 import Player from '../components/common/Player.js'
 
-const teamPlayer = (offsetX, offsetY, rotate=0) => ({
-  x,
-  y,
-  onClick,
-  selected,
-  ...props
-}) => (
-  <Transform 
-    selected={selected}
-    onClick={onClick}
-    x={x - offsetX} 
-    y={y - offsetY} 
-    rotate={rotate} 
-    scale={0.03}>
-    <Player {...props} />
-  </Transform>
+const teamPlayer = (offsetX, offsetY, rotate=0) => ({x, y, ...props}) => (
+  <Draggable {...props}>
+    <Transform {...props}
+      x={x - offsetX} 
+      y={y - offsetY} 
+      rotate={rotate} 
+      scale={0.03}>
+      <Player {...props} />
+    </Transform>
+  </Draggable>
 )
 
 export default teamPlayer
